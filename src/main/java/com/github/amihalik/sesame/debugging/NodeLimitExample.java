6a26045d89ebee1c72cc0452e2b01dc29544b85c
Aaron Mihalik <aaron.mihalik@gmail.com>
2017-02-23 07:00:36 -0500

    Adding NodeLimitExample

---
+++ src/main/java/com/github/amihalik/sesame/debugging/NodeLimitExample.java
@@ -0,0 +1,55 @@
+package com.github.amihalik.sesame.debugging;
+
+import static com.github.amihalik.sesame.debugging.Utils.evaluateAndCount;
+import static com.github.amihalik.sesame.debugging.Utils.evaluateAndPrint;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+
+public class NodeLimitExample {
+    
+    public static void main(String[] args) throws Exception {
+        // The OpenRDF In Memory Sail Repo doesn't seem to behave as expected.
+        //SailRepositoryConnection conn = Utils.getInMemoryConn();
+
+        
+        // The Accumulo In Memory Rya Sail Repo behaves as expected.
+        SailRepositoryConnection conn = Utils.getInMemoryAccConn();
+
+        
+        ValueFactory vf = new ValueFactoryImpl();
+        
+        URI personUri = vf.createURI("t:person");
+        URI talksTo = vf.createURI("p:talksTo");
+        
+        List<Statement> people = IntStream.iterate(1, i -> i + 1).limit(100)
+                                                   .mapToObj(i -> vf.createStatement(vf.createURI("s:"+String.format("%03d",i)), RDF.TYPE, personUri))
+                                                   .collect(toList());
+        
+        Stream<Statement> talkStream = people.stream().map(Statement::getSubject)
+                                                   .flatMap(subj -> people.stream().map(statement -> vf.createStatement(subj, talksTo, statement.getSubject() )));
+        System.out.println(people.size());
+        conn.add(people);
+        conn.add(talkStream::iterator);
+
+        String sparql = "SELECT * WHERE {  " + 
+                        "   { SELECT * WHERE { ?s a <t:person> } LIMIT 10 } " + 
+                        "   { SELECT * WHERE { ?s <p:talksTo> ?o . ?o a <t:person> } LIMIT 12 } " + 
+                        "}";
+        System.out.println(conn.prepareTupleQuery(QueryLanguage.SPARQL, sparql));
+        
+        evaluateAndPrint(sparql, conn);
+        evaluateAndCount(sparql, conn);
+    }
+
+}
---
+++ src/main/java/com/github/amihalik/sesame/debugging/Utils.java
@@ -0,0 +1,97 @@
+package com.github.amihalik.sesame.debugging;
+
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.rya.accumulo.AccumuloRdfConfiguration;
+import org.apache.rya.api.RdfCloudTripleStoreConfiguration;
+import org.apache.rya.api.persist.RyaDAOException;
+import org.apache.rya.indexing.GeoConstants;
+import org.apache.rya.indexing.accumulo.ConfigUtils;
+import org.apache.rya.indexing.external.PrecomputedJoinIndexerConfig;
+import org.apache.rya.indexing.external.PrecomputedJoinIndexerConfig.PrecomputedJoinStorageType;
+import org.apache.rya.rdftriplestore.inference.InferenceEngineException;
+import org.apache.rya.sail.config.RyaSailFactory;
+import org.openrdf.model.Statement;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.QueryResultHandlerException;
+import org.openrdf.query.TupleQueryResultHandlerException;
+import org.openrdf.query.resultio.helpers.QueryResultCollector;
+import org.openrdf.query.resultio.text.csv.SPARQLResultsCSVWriter;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+import org.openrdf.sail.Sail;
+import org.openrdf.sail.SailException;
+import org.openrdf.sail.memory.MemoryStore;
+
+public class Utils {
+
+    public static SailRepositoryConnection getInMemoryConn() throws RepositoryException {
+        SailRepository s = new SailRepository(new MemoryStore());
+        s.initialize();
+        SailRepositoryConnection conn = s.getConnection();
+        return conn;
+    }
+
+    public static SailRepositoryConnection getInMemoryAccConn()
+            throws RepositoryException, SailException, AccumuloException, AccumuloSecurityException, RyaDAOException, InferenceEngineException {
+        Configuration conf = getConf();
+        conf.setBoolean(ConfigUtils.DISPLAY_QUERY_PLAN, true);
+
+        SailRepository repository = null;
+        SailRepositoryConnection conn = null;
+
+        final Sail extSail = RyaSailFactory.getInstance(conf);
+        repository = new SailRepository(extSail);
+        conn = repository.getConnection();
+        return conn;
+    }
+
+    public static void evaluateAndPrint(String sparqlQuery, SailRepositoryConnection conn)
+            throws TupleQueryResultHandlerException, QueryEvaluationException, MalformedQueryException, RepositoryException {
+        conn.prepareTupleQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(new SPARQLResultsCSVWriter(System.out));
+
+    }
+
+    public static void evaluateAndCount(String sparqlQuery, SailRepositoryConnection conn)
+            throws QueryEvaluationException, MalformedQueryException, RepositoryException, QueryResultHandlerException {
+        QueryResultCollector c = new QueryResultCollector();
+        conn.prepareTupleQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(c);
+        System.out.println("Result Count :: " + c.getBindingSets().size());
+
+    }
+
+    public static void printRepo(SailRepositoryConnection conn) throws RepositoryException {
+        RepositoryResult<Statement> rr = conn.getStatements(null, null, null, false);
+        while (rr.hasNext()) {
+            System.out.println(rr.next());
+        }
+        System.out.println("Repo size :: " + conn.size());
+
+    }
+
+    private static Configuration getConf() {
+
+        final AccumuloRdfConfiguration conf = new AccumuloRdfConfiguration();
+
+        conf.setBoolean(ConfigUtils.USE_MOCK_INSTANCE, true);
+        conf.set(ConfigUtils.USE_PCJ, "true");
+        conf.set(ConfigUtils.USE_FREETEXT, "true");
+        conf.set(ConfigUtils.USE_TEMPORAL, "true");
+        conf.set(PrecomputedJoinIndexerConfig.PCJ_STORAGE_TYPE, PrecomputedJoinStorageType.ACCUMULO.name());
+        conf.set(RdfCloudTripleStoreConfiguration.CONF_TBL_PREFIX, "RYA_");
+        conf.set(ConfigUtils.CLOUDBASE_USER, "root");
+        conf.set(ConfigUtils.CLOUDBASE_PASSWORD, "");
+        conf.set(ConfigUtils.CLOUDBASE_INSTANCE, "instance");
+        conf.setInt(ConfigUtils.NUM_PARTITIONS, 3);
+        conf.set(ConfigUtils.CLOUDBASE_AUTHS, "U");
+
+        // only geo index statements with geo:asWKT predicates
+        conf.set(ConfigUtils.GEO_PREDICATES_LIST, GeoConstants.GEO_AS_WKT.stringValue());
+        return conf;
+    }
+}
