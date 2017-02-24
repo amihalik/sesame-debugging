f8abd3546b95af9b308c098793f17a89f938c036
Aaron Mihalik <mihalik@alum.mit.edu>
2017-02-24 09:10:51 -0500

    Adding docs

--- src/main/java/com/github/amihalik/sesame/debugging/PropertyPathsExample.java
+++ src/main/java/com/github/amihalik/sesame/debugging/PropertyPathsExample.java
@@ -12,47 +12,53 @@ import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.repository.sail.SailRepositoryConnection;
 
+/*
+ * There are many more property path examples at: http://stackoverflow.com/a/26707541
+ */
+
 public class PropertyPathsExample {
 
     public static void main(String[] args) throws Exception {
         // The OpenRDF In Memory Sail Repo
-//        SailRepositoryConnection conn = Utils.getInMemoryConn();
+        // SailRepositoryConnection conn = Utils.getInMemoryConn();
 
         // The Accumulo In Memory Rya Sail Repo
-         SailRepositoryConnection conn = Utils.getInMemoryAccConn();
+        SailRepositoryConnection conn = Utils.getInMemoryAccConn();
 
-        
         int depth = 10;
-        
+
         ValueFactory vf = new ValueFactoryImpl();
 
         URI causes = vf.createURI("p:causes");
-        
+
         final Random r = new Random(1);
-        
+
         IntFunction<URI> createUri = i -> vf.createURI("s:" + String.format("%03d", i));
-        
-        IntFunction<Stream<Statement>> createEventChain = i ->  {
+
+        // for every positive int k that is less than i, randomly create a "caused by" relation between k and i
+        IntFunction<Stream<Statement>> createPrecedingEventLink = i -> {
             return IntStream.iterate(1, k -> k + 1)
-                     .limit(i-1)
-                     .filter(k -> r.nextFloat() > .5)
-                     .mapToObj(k ->  vf.createStatement(createUri.apply(k), causes, createUri.apply(i)));
+                            .limit(i - 1)
+                            .filter(k -> r.nextFloat() > .5)
+                            .mapToObj(k -> vf.createStatement(createUri.apply(k), causes, createUri.apply(i)));
         };
-        
-        Stream<Statement> events = IntStream.iterate(1, i -> i + 1).limit(depth)
-                                        .mapToObj(createEventChain)
-                                        .flatMap(i->i);
+
+        Stream<Statement> events = IntStream.iterate(1, i -> i + 1)
+                                            .limit(depth)
+                                            .mapToObj(createPrecedingEventLink)
+                                            .flatMap(i -> i);
 
         conn.add(events::iterator);
-        
-//        Utils.printRepo(conn);
+
+        // Utils.printRepo(conn);
 
         String sparql = "SELECT DISTINCT ?s ?o WHERE {  " 
-                        + "    VALUES ?root { <s:001> }"
-                        + "    { ?root (<p:causes>*) ?s  . ?s <p:causes> ?o }"
-                        + "      UNION  "
-                        + "    { ?root (^<p:causes>*)  ?o . ?s <p:causes> ?o }"
-                        + " }";
+                      + "    VALUES ?root { <s:001> }"
+                      + "    { ?root (<p:causes>*) ?s  . ?s <p:causes> ?o }" 
+                      + "      UNION  "
+                      + "    { ?root (^<p:causes>*)  ?o . ?s <p:causes> ?o }" 
+                      + " }" 
+                      + " ORDERBY ?s";
 
         System.out.println(conn.prepareTupleQuery(QueryLanguage.SPARQL, sparql));
 
