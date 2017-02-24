0225763ddf6e858896fd559f01add19f7e6da537
Aaron Mihalik <mihalik@alum.mit.edu>
2017-02-24 08:59:07 -0500

    Adding PropertyPathsExample

---
+++ src/main/java/com/github/amihalik/sesame/debugging/PropertyPathsExample.java
@@ -0,0 +1,62 @@
+package com.github.amihalik.sesame.debugging;
+
+import java.util.Random;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+
+public class PropertyPathsExample {
+
+    public static void main(String[] args) throws Exception {
+        // The OpenRDF In Memory Sail Repo
+//        SailRepositoryConnection conn = Utils.getInMemoryConn();
+
+        // The Accumulo In Memory Rya Sail Repo
+         SailRepositoryConnection conn = Utils.getInMemoryAccConn();
+
+        
+        int depth = 10;
+        
+        ValueFactory vf = new ValueFactoryImpl();
+
+        URI causes = vf.createURI("p:causes");
+        
+        final Random r = new Random(1);
+        
+        IntFunction<URI> createUri = i -> vf.createURI("s:" + String.format("%03d", i));
+        
+        IntFunction<Stream<Statement>> createEventChain = i ->  {
+            return IntStream.iterate(1, k -> k + 1)
+                     .limit(i-1)
+                     .filter(k -> r.nextFloat() > .5)
+                     .mapToObj(k ->  vf.createStatement(createUri.apply(k), causes, createUri.apply(i)));
+        };
+        
+        Stream<Statement> events = IntStream.iterate(1, i -> i + 1).limit(depth)
+                                        .mapToObj(createEventChain)
+                                        .flatMap(i->i);
+
+        conn.add(events::iterator);
+        
+//        Utils.printRepo(conn);
+
+        String sparql = "SELECT DISTINCT ?s ?o WHERE {  " 
+                        + "    VALUES ?root { <s:001> }"
+                        + "    { ?root (<p:causes>*) ?s  . ?s <p:causes> ?o }"
+                        + "      UNION  "
+                        + "    { ?root (^<p:causes>*)  ?o . ?s <p:causes> ?o }"
+                        + " }";
+
+        System.out.println(conn.prepareTupleQuery(QueryLanguage.SPARQL, sparql));
+
+        Utils.evaluateAndPrint(sparql, conn);
+        Utils.evaluateAndCount(sparql, conn);
+    }
+}
